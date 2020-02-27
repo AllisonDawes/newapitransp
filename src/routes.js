8b8767e5062c7fbe740dfd52e2dae463015847e0
otabol <allyssondawes@hotmail.com>
2020-02-26 22:51:24 -0300

    config shield

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,7 @@
+import "dotenv/config";
 import Router from "express";
+import Brute from "express-brute";
+import BruteRedis from "express-brute-redis";
 
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
@@ -12,8 +15,15 @@ import authMiddleware from "./app/middlewares/auth";
 
 const routes = new Router();
 
+const bruteStore = new BruteRedis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT
+});
+
+const bruteForce = new Brute(bruteStore);
+
 routes.post("/users", UserController.store);
-routes.post("/session", SessionController.store);
+routes.post("/session", bruteForce.prevent, SessionController.store);
 
 routes.use(authMiddleware);
 
