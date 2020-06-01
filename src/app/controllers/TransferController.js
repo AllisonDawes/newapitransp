2ba64bef7fd70b4aed278e26dc9dac3a053c767f
AllisonDawes <allyssondawes@hotmail.com>
2020-05-31 23:34:41 -0300

    set log store

--- src/app/controllers/TransferController.js
+++ src/app/controllers/TransferController.js
@@ -80,6 +80,7 @@ class Transfer {
     const net_weight = weight_brute - carweight.car_weight;
 
     console.log(startDay);
+    console.log(tzStartDay);
 
     const transfer = await TransferModel.create({
       user_id: req.userId,
