c5c0a0aad70a581e06b1a900064557e5ad402547
AllisonDawes <allyssondawes@hotmail.com>
2020-07-13 22:51:03 -0300

    inclusão filtro personalizado

--- src/app/controllers/FilterTransferController.js
+++ src/app/controllers/FilterTransferController.js
@@ -5,19 +5,40 @@ import { eachDayOfInterval, parseISO } from "date-fns";
 
 class FilterTransferController {
   async index(req, res) {
-    const { dateStart, dateEnd } = req.body;
-    const { page = 1 } = req.query;
+    const { dateStart, dateEnd } = req.query;
+
+    console.log(dateStart);
+    console.log(dateEnd);
+
+    if (!dateStart || !dateEnd) {
+      return res
+        .status(401)
+        .json({ error: "Informe a data inicial e final da pesquisa." });
+    }
 
     const intervalDate = eachDayOfInterval({
       start: parseISO(dateStart),
       end: parseISO(dateEnd),
     });
 
-    const filterTransfer = await TransferModel.findAndCountAll({
+    const filterTransfer = await TransferModel.findAll({
       where: { date: intervalDate },
+      attributes: [
+        "id",
+        "user_id",
+        "carweight_id",
+        "weight_brute",
+        "car_weight",
+        "date",
+        "mat_type",
+        "mat_origin",
+        "mat_dest",
+        "weight",
+        "comments",
+        "created_at",
+        "updated_at",
+      ],
       order: ["created_at"],
-      limit: 31,
-      offset: (page - 1) * 31,
       include: [
         {
           model: User,
